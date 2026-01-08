d55ac6dd5bbe4429220275e05219276fea9b2fdc
gitcoder-amit <yadavamit14801@gmail.com>
2026-01-08 23:53:04 +0530

    added css for Public Layout

---
+++ FoodOrderingSystem/frontend/src/components/PublicLayout.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaHome, FaSignInAlt, FaTruck, FaUserPlus, FaUserShield, FaUtensils } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import '../styles/layout.css'
+
+const PublicLayout = ({children}) => {
+    return (
+        <div>
+           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand fw-bold" to="#"><FaUtensils className='me-1'/>Food Ordering System</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2">
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" aria-current="page" to="#"><FaHome className='me-1'/>Home</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" to="#"><FaUtensils className='me-1'/>Menu</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" to="#"><FaTruck className='me-1'/>Track</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" to="#"><FaUserPlus className='me-1'/>Register</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" to="#"><FaSignInAlt className='me-1'/>Login</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                        <Link className="nav-link" to="#"><FaUserShield className='me-1'/>Admin Login</Link>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div>{children}</div>
+
+            <footer className='text-center py-3 mt-5'>
+                <div className="container">
+                    <p className=''>&copy; 2026 Food Ordering System. All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default PublicLayout;
--- FoodOrderingSystem/frontend/src/pages/Home.js
+++ FoodOrderingSystem/frontend/src/pages/Home.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaTruck, FaUserPlus, FaUserShield, FaUtensils } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import PublicLayout from '../components/PublicLayout';
 
 
 const Home = () => {
     return (
-        <div>
-           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container">
-                    <Link className="navbar-brand fw-bold" to="#"><FaUtensils className='me-1'/>Food Ordering System</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2">
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link active" aria-current="page" to="#"><FaHome className='me-1'/>Home</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link" to="#"><FaUtensils className='me-1'/>Menu</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link" to="#"><FaTruck className='me-1'/>Track</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link" to="#"><FaUserPlus className='me-1'/>Register</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link" to="#"><FaSignInAlt className='me-1'/>Login</Link>
-                        </li>
-                        <li className="nav-item mx-1">
-                        <Link className="nav-link" to="#"><FaUserShield className='me-1'/>Admin Login</Link>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <PublicLayout>
+            <div>
+                Home
+            </div>
+        </PublicLayout>
     )
 }
 
